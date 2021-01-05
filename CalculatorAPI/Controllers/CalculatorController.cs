19042ef20741b02d19b3b1d9085768858e7a5466
TheEsquimo <elvostudios@hotmail.com>
2021-01-05 12:22:35 +0100

    Added calculator API

---
+++ CalculatorAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorAPI
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController
+    {
+        [HttpGet]
+        public double[] GetPercentagesOfTotal(int a, int b)
+        {
+            return Calculator.GetPercentagesOfTotal(a, b);
+        }
+    }
+}
---
+++ CalculatorAPI/Utils/Calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorAPI
+{
+    public static class Calculator
+    {
+        public static double[] GetPercentagesOfTotal(int a, int b)
+        {
+            if (a < 0 || b < 0)
+                throw new ArgumentOutOfRangeException("Negative numbers are invalid");
+
+            int total = a + b;
+            double[] percentages;
+
+            if (total == 0)
+                percentages = GetZeroPercentageArray();
+            else if (a > 0 && b > 0)
+                percentages = CalculatePercentage(a, b, total);
+            else
+                percentages = GetHundredPercentArray(a, b);
+
+            return percentages;
+        }
+
+        private static double[] GetZeroPercentageArray()
+        {
+            return new double[]
+            {
+                0,
+                0
+            };
+        }
+
+        private static double[] CalculatePercentage(int a, int b, int total)
+        {
+            return new double[]
+            {
+                Math.Round(((double)a / total) * 100, 0),
+                Math.Round(((double)b / total) * 100, 0)
+            };
+        }
+
+        private static double[] GetHundredPercentArray(int a, int b)
+        {
+            double[] percentageArray;
+            if (a > 0)
+                percentageArray = new double[] { 100, 0 };
+            else
+                percentageArray = new double[] { 0, 100 };
+
+            return percentageArray;
+        }
+    }
+}
