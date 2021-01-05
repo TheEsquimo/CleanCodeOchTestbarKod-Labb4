10f4a59ab48c30145bb575648c2a9a8627ec7ecb
TheEsquimo <elvostudios@hotmail.com>
2021-01-05 15:23:56 +0100

    Added magic card models

---
+++ MagicTheVotingAPI/Models/MagicCard.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicTheVotingAPI
+{
+    public class MagicCard
+    {
+        [Key]
+        public int MultiverseId { get; set; }
+    }
+}
---
+++ MagicTheVotingAPI/Models/MagicVotePair.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MagicTheVotingAPI
+{
+    public class MagicVotePair
+    {
+        public Guid Id { get; set; }
+        public MagicCard CardA { get; set; }
+        public MagicCard CardB { get; set; }
+        public int CardAVotes { get; set; }
+        public int CardBVotes { get; set; }
+    }
+}
