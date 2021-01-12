c9f155c49e8fa559f9d1c0ffb7c927a61ee8e4a8
Sunse7 <sunsetluna@gmail.com>
2021-01-12 11:02:32 +0100

    Test that magic quote does not exist before card click

--- e2etests/cypress/integration/votingTests.spec.js
+++ e2etests/cypress/integration/votingTests.spec.js
@@ -1,6 +1,7 @@
 describe('Test', () => {
     it('Test to click a card', () => {
         cy.visit("http://localhost").
+            get('#magic-quote').should('not.exist').
             get('#card-a').click().
             get('#magic-quote').should('exist').
             get('#next-button').click()
